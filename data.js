280b77cc1a3f54df12aaf1903c705faaa52292c0
Anurag Vaibhav <85223862+itisia@users.noreply.github.com>
2021-09-27 09:40:23 +0530

    updated data.js
    just added some methods to manipulate the database

--- data.js
+++ data.js
@@ -1,16 +1,50 @@
 let classes={
-	_data:{},
+	_data:{
+	},
 	listYears(){
-
+		let a=[];
+		for(let d in this._data){
+			a.push(d);
+		}
+		return a;
 	},
 	listMonths(year){
-
+		let y=this.listYears();
+		if(y.indexOf(`${year}`)!=-1){
+			let d=[];
+			for(let a in this._data[`${year}`]){
+				d.push(a);
+			}
+			return d;
+		}else{
+			return false;
+		}
 	},
-	listDaya(year,month){
-
+	listDays(year,month){
+		let m=this.listMonths(year);
+		if(m!=false){
+			if(m.length===0){
+				return [];
+			}else{
+				if(month>0){
+					let a=[];
+				for(let d in this._data[`${year}`][`${month}`]){
+					a.push(d)
+				}
+				return a;
+				}
+			}
+		}else{
+			return false;
+		}
 	},
-	listLecs(year,month,day){
+	showSingleLec(year,month,day){
+		let d=this.listDays(year,month);
+		if(d.indexOf(`${day}`)!=-1){
 
+		}else{
+			return false;
+		}
 	},
 	showAllSubs(){
 
@@ -22,6 +56,28 @@ let classes={
 
 	},
 	createNewDay(){
-
-	}
+		
+	},
+	createNewYear(year){
+		if(this.listYears().indexOf(`${year}`)!=-1){
+			return false;
+		}else{
+			this._data[`${year}`]={};
+			return true;
+		}
+	},
+	createNewMonth(year,month){
+		if(this.listYears().indexOf(`${year}`)!=-1){
+			if(this.listMonths(year).indexOf(`${month}`)!=-1){
+				return false;
+			}else{
+				this._data[`${year}`][`${month}`]={};
+				return true;
+			}
+		}else{
+			/* if the year not exist */
+			console.log(`Year ${year} Not Exist`);
+			return false;
+		}
+	},
 };
